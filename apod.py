3109278f9b41184446044ff8f2d870c95179e8e7
Christopher Bradshaw <christopher.peter.bradshaw+github@gmail.com>
2018-04-11 15:04:45 -0700

    python3ify it

--- apod.py
+++ apod.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # Attempts to mimic apod.sh
 
@@ -9,19 +9,19 @@ from bs4 import BeautifulSoup
 
 # Retrieve webpage
 filename = "x" # filename = tmp stopped working for some reason? tmp was dir???
-urllib.urlretrieve("http://apod.nasa.gov/apod/astropix.html", filename)
+urllib.request.urlretrieve("http://apod.nasa.gov/apod/astropix.html", filename)
 
 f = open(filename, "r")
 data = [line for line in f]
-soup = BeautifulSoup(''.join(data))
+soup = BeautifulSoup(''.join(data), "html5lib")
 os.remove(filename)
 
 try:
-	src = soup.body.find_all("a")[1].img["src"]
-	if not src:
-		sys.exit()
+    src = soup.body.find_all("a")[1].img["src"]
+    if not src:
+        sys.exit()
 except:
-	sys.exit()
+    sys.exit()
 
 # Found the line in which the name is. Extract it
 location = "http://apod.nasa.gov/apod/" + src
@@ -32,7 +32,7 @@ image_dir = os.path.realpath(__file__).rsplit("/", 1)[0] + "/images/"
 
 # If we do not have the image, get it.
 if file_name not in os.listdir(image_dir):
-	urllib.urlretrieve(location, image_dir + file_name)
+    urllib.request.urlretrieve(location, image_dir + file_name)
 
 # set images as background
 image = "file://" + image_dir + file_name
